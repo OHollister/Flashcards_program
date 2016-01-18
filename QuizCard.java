b324561710ae1a22d9af9b5b8d8172531deb63c1
Olga Hollister <olya778899@gmail.com>
2016-01-18 12:07:49 -0500

    Create QuizCard.java

---
+++ QuizCard.java
@@ -0,0 +1,25 @@
+/*A class representing card data.*/
+
+public class QuizCard {
+
+	    private String question;
+	    private String answer;
+		public QuizCard(String question, String answer){
+			this.question=question;
+			this.answer=answer;
+			
+		}
+		
+		public void setQuestion(String question){
+			this.question=question;
+		}
+       
+		public String getQuestion(){
+			return question;
+		}
+			 public void setAnswer(String answer){
+			 this.answer=answer;
+		 }
+		public String getAnswer(){
+			return answer;
+		}}
