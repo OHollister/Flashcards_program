2106fde5e58c6e1e09a87c211080690f1a386b88
Olga Hollister <olya778899@gmail.com>
2016-01-18 12:08:20 -0500

    Create ReadFile.java

---
+++ ReadFile.java
@@ -0,0 +1,22 @@
+import java.io.*;
+class ReadFile {
+	public static void main (String[]args){
+		try{
+			File myFile =new File("");
+			FileReader fileReader =new FileReader(myFile);
+			
+			BufferedReader reader =new BufferedReader(fileReader) ;
+			
+		//make a String variable to hold each line as the line is read
+			
+			String line =null;
+			
+			while ((line =reader.readLine())!=null){
+				System.out.println(line);
+			}
+			reader.close();
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}
+	}
+ }
