61240a180b1ea69a6794f7f0e5dce55984fa7864
Olga Hollister <OHollister@users.noreply.github.com>
2017-01-10 20:25:30 -0500

    Update ReadFile.java

--- ReadFile.java
+++ ReadFile.java
@@ -6,7 +6,6 @@ class ReadFile {
 			FileReader fileReader =new FileReader(myFile);
 			
 			BufferedReader reader =new BufferedReader(fileReader) ;
-			
 		//make a String variable to hold each line as the line is read
 			
 			String line =null;
